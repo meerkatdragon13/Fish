d9b0bc1c8674e19a87c390985d090756e66890a6
meerkatdragon13 <155834845+meerkatdragon13@users.noreply.github.com>
2026-08-09 18:01:18 -0500

    Create Main.cpp
    Need to rewrite lines 6 - 8

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cerr << "Started software.\n";
+  system("echo Fishsticks is amazing");
+  system("mkdir Hello");
+  system("touch Hello/HI");
+  cerr << "Shutting down software.\n";
+  return 0;
+}
